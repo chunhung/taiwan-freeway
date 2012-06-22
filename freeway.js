5e1a5b908001201fa911c76ab36f0c42a30d0bdf
Michael Chen <arnose@gmail.com>
2012-06-22 23:29:46 +0800

    Add the simple version of freeway-taiwan

---
+++ freeway.js
@@ -0,0 +1,76 @@
+  var cur_area = 'n';
+  var position = 'right';
+  var cur_size = 2;
+  var cur_mins = 0;
+
+  if (window.addEventListener) {
+    //window.addEventListener('load', map_url, false);
+    window.onload = map_url(cur_area);
+  }
+
+  function map_url(area) {
+    cur_area = area;
+    var d = new Date();
+    var minutes = parseInt(d.getMinutes());
+    if ( minutes == 0 )
+      minutes = 59;
+    else
+      minutes = minutes - 1;
+    if ( cur_mins != minutes )
+      cur_mins = minutes;
+    var map = document.getElementById('freeway');
+    map.src = 'http://pda.freeway.gov.tw/m/map/map_'+area+'_'+cur_size+'_'+minutes+'.jpg#e';
+    show_section((area!='n'), (area!='c'), (area!='s'));
+  };
+
+  function show_section(n,c,s) {
+    document.getElementById('n').style.display = (n)?'block':'none';
+    document.getElementById('c').style.display = (c)?'block':'none';
+    document.getElementById('s').style.display = (s)?'block':'none';
+  }
+
+  function show_control(small, large) {
+    if ( small )
+      document.getElementById('small').style.display = 'block';
+    else
+      document.getElementById('small').style.display = 'none';
+
+    if ( large )
+      document.getElementById('large').style.display = 'block';
+    else
+      document.getElementById('large').style.display = 'none';
+  }
+
+  function change_size(size) {
+    if ( size == '-' && cur_size > 1 ) {
+      cur_size = cur_size - 1;
+      map_url(cur_area);
+      show_control((cur_size!=1), true);
+    } else if ( size == '+' && cur_size < 3 ) {
+      cur_size = cur_size + 1;
+      map_url(cur_area);
+      show_control(true, (cur_size!=3));
+    }
+  }
+
+  function change_position() {
+    var position = document.getElementById('position');
+    var selection = document.getElementById('selection');
+    if ( position.getAttribute('class') == 'pos_right' ) {
+      position.setAttribute('class', 'pos_left');
+      selection.setAttribute('class', 'selection_right');
+    } else {
+      position.setAttribute('class', 'pos_right');
+      selection.setAttribute('class', 'selection_left');
+    }
+  }
+
+  if ( document.body.addEventListener ) {
+    document.getElementById('n').addEventListener("click", function(){map_url(this.id)}, false);
+    document.getElementById('c').addEventListener("click", function(){map_url(this.id)}, false);
+    document.getElementById('s').addEventListener("click", function(){map_url(this.id)}, false);
+    document.getElementById('freeway').addEventListener("click", function(){map_url(cur_area)}, false);
+    document.getElementById('position').addEventListener("click", change_position, false);
+    document.getElementById('small').addEventListener("click", function(){change_size('-');}, false);
+    document.getElementById('large').addEventListener("click", function(){change_size('+');}, false);
+  }
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+//  if ( date_default_timezone_set('Asia/Taipei') ) {
+//    $datetime = getdate();
+//    $minutes = $datetime['minutes'];
+//    if ( intval($minutes) == 0 )
+//      $minutes = '59';
+//    else
+//      $minutes = intval($minutes)-1;
+//  }
+?>
+
+<html>
+<head>
+    <meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" type="text/css" href="./freeway.css" />
+  <title>高速公路</title>
+</head>
+<body>
+<div id="section" class="section">
+  <div id="selection" class="selection_left">
+    <span id="n" name="section" class="area">北</span>
+    <span id="c" name="section" class="area">中</span>
+    <span id="s" name="section" class="area">南</span>
+    <span id="small" name="size" class="area">-</span>
+    <span id="large" name="size" class="area">+</span>
+  </div>
+  <div id="position" class="pos_right">
+    <span id="pos_button" class="pos_button">＊</span>
+  </div>
+  <div id="map" class="map">
+    <img id='freeway'></img>
+  </div>
+</div>
+</body>
+
+<script src='./freeway.js' type='text/javascript'></script>
+
+</html>
