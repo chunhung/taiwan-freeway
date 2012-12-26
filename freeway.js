f9ce6b5e17dd6f087bba9785f5592d08ff593e64
Arnose <arnose@gmail.com>
2012-12-26 13:54:20 +0800

    Use Event Handler to Handle the Click on the Text =================================================
    * User can click on the text to filter the incidents
    * These clicks are incrementally filtered
    * Use Event Handler to be Passed into the function to acquire essential information

--- freeway.js
+++ freeway.js
@@ -24,8 +24,20 @@
         }
         show('reset');
         if ( document.body.addEventListener ) {
-            document.getElementById('button_reset').addEventListener("click", function(){show('reset');}, false);
-            document.getElementById('button_search').addEventListener("click", function(){show('filter');}, false);
+            document.getElementById('button_reset').addEventListener("click", function(){show('reset', null);}, false);
+            document.getElementById('button_search').addEventListener("click", function(){show('filter', null);}, false);
+            var to = document.getElementsByName('to');
+            var road = document.getElementsByName('road');
+            var inc = document.getElementsByName('inc');
+            for(var i = 0; i < to.length; i++) {
+                to[i].addEventListener("click", function(click){show('filter', click)}, false);
+            }
+            for(var j = 0; j < road.length; j++) {
+                road[j].addEventListener("click", function(click){show('filter', click);}, false);
+            }
+            for(var k = 0; k < inc.length; k++) {
+                inc[k].addEventListener("click", function(click){show('filter', click);}, false);
+            }
         }
     }
 
@@ -108,54 +120,29 @@
         document.getElementById('button_search').disabled = true;
     }
 
-    function show(type) {
-        if ( type == 'reset' ) {
+    function show(status, click) {
+        if ( status == 'reset' ) {
             change_incident_display('全部', '全部', '全部');
-            /*
-            var tbody = document.getElementsByTagName('tbody');
-            var tr = tbody[0].getElementsByTagName('tr');
-            for ( var i = 0; i < tr.length; i++ ) {
-                if ( tr[i].hasAttribute('class') ) {
-                    var tr_class = tr[i].getAttribute('class');
-                    if ( tr_class.indexOf('hide') > -1 ) {
-                        tr_class = tr_class.replace('hide', 'show');
-                    } else if ( tr_class.indexOf('show') == -1 ) {
-                        tr_class = tr_class + ' show';
-                    }
-                        tr[i].setAttribute('class', tr_class);
-                } else {
-                    tr[i].setAttribute('class', 'show');
-                }
-            }*/
+
             document.getElementById('select_to').value='全部';
             document.getElementById('select_road').value='全部';
             document.getElementById('select_inc').value='全部';
-            document.getElementById('button_search').disabled = false;
-        } else if ( type == 'filter' ){
-            var to = document.getElementById('select_to').value;
-            var road = document.getElementById('select_road').value;
-            var inc = document.getElementById('select_inc').value;
 
-            change_incident_display(to, road, inc);
-            /*
-            var tbody = document.getElementsByTagName('tbody');
-            var tr = tbody[0].getElementsByTagName('tr');
-            for ( var i = 0; i < tr.length; i++ ) {
-                var status = 'show';
-                var spans = tr[i].getElementsByTagName('span');
-                for ( var j = 0; j < spans.length; j++ ) {
-                    var span_class = spans[j].getAttribute('class');
-                    var span_name = spans[j].getAttribute('Name');
-                    if ( to != '全部' && span_name == 'to' && status == 'show' )
-                        status = ( span_class == to )?'show':'hide';
-                    if ( road != '全部' && span_name == 'road' && status == 'show' )
-                        status = ( span_class == road )?'show':'hide';
-                    if ( inc != '全部' && span_name == 'inc' && status == 'show' )
-                        status = ( span_class == inc )?'show':'hide';
-                    tr[i].setAttribute('class', status);
-                }
+            document.getElementById('button_search').disabled = false;
+        } else if ( status == 'filter' ){
+            var filter = Array();
+            filter['to'] = document.getElementById('select_to').value;
+            filter['road'] = document.getElementById('select_road').value;
+            filter['inc'] = document.getElementById('select_inc').value;
+
+            if ( click != null ) {
+                filter[ click.srcElement.getAttribute('name') ] = click.srcElement.getAttribute('class');
+                document.getElementById('select_'+click.srcElement.getAttribute('name')).value=click.srcElement.getAttribute('class');
             }
-            document.getElementById('button_search').disabled = true;*/
+
+            change_incident_display(filter['to'], filter['road'], filter['inc']);
+
+            document.getElementById('button_search').disabled = true;
         }
     }
 
